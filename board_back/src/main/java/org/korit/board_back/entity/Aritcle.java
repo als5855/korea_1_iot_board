18a263f5e095459b83f7f5dc4555e4e6b2018e3d
als5855 <wlals5855@naver.com>
2024-11-07 17:14:36 +0900

    20241107-2

--- board_back/src/main/java/org/korit/board_back/entity/Aritcle.java
+++
@@ -1,4 +0,0 @@
-package org.korit.board_back.entity;
-
-public class Aritcle {
-}
--- board_back/src/main/java/org/korit/board_back/entity/User.java
+++ board_back/src/main/java/org/korit/board_back/entity/User.java
@@ -1,5 +1,38 @@
 package org.korit.board_back.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "Users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String userId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(columnDefinition = "ENUM('M', 'F')")
+    private String gender;
 }
